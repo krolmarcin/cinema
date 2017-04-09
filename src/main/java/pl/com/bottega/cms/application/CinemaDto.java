1ed5b6d6529ebbba5bd8da078b881a05ea8d313d
ogurekk <karwacki.wojciech@gmail.com>
2017-04-09 13:54:45 +0200

    cinema catalog -> getCinemas()

--- src/main/java/pl/com/bottega/cms/application/CinemaDto.java
+++ src/main/java/pl/com/bottega/cms/application/CinemaDto.java
@@ -14,21 +14,27 @@ public class CinemaDto {
     private String name;
     private String city;
 
-    private Set<Movie> movies;
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getCity() {
         return city;
     }
 
-    public Set<Movie> getMovies() {
-        return movies;
+    public void setCity(String city) {
+        this.city = city;
     }
 }
