d4882921b2efe63aa259ba8c6671fa3daf4cd6e5
Marcin <szeryfmk@gmail.com>
2017-04-09 15:01:26 +0200

    validator

---
+++ src/main/java/pl/com/bottega/cms/model/commands/Validatable.java
@@ -0,0 +1,36 @@
+package pl.com.bottega.cms.model.commands;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public interface Validatable {
+
+    void validate(ValidationErrors errors);
+
+    default boolean isEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
+    class ValidationErrors {
+
+        private Map<String, Set<String>> errors = new HashMap<>();
+
+        public void add(String fieldName, String errorMessage) {
+            Set<String> fieldErrors = errors.getOrDefault(fieldName, new HashSet<>());
+            fieldErrors.add(errorMessage);
+            errors.putIfAbsent(fieldName, fieldErrors);
+        }
+
+        public boolean isValid() {
+            return errors.isEmpty();
+        }
+
+        public Map<String, Set<String>> getErrors() {
+            return errors;
+        }
+
+    }
+
+}
