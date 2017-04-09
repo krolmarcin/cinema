f5d4baed1efe5a373dcc15d6490ac69f87cbb365
Marcin <szeryfmk@gmail.com>
2017-04-09 15:57:22 +0200

    validator

--- src/main/java/pl/com/bottega/cms/model/CinemaRepository.java
+++ src/main/java/pl/com/bottega/cms/model/CinemaRepository.java
@@ -4,6 +4,11 @@ package pl.com.bottega.cms.model;
  * Created by maciek on 09.04.2017.
  */
 public interface CinemaRepository {
+
     void put(Cinema c);
+
     Cinema get(Long id);
+
+    boolean exists(String name, String city);
+
 }
