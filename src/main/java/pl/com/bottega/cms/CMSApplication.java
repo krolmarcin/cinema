9bdccce676b7496cd77152b8b9304ac454661ec7
ogurekk <karwacki.wojciech@gmail.com>
2017-04-09 09:27:30 +0200

    podstawowy commit projektu

--- src/main/java/pl/com/bottega/cms/CMSApplication.java
+++ src/main/java/pl/com/bottega/cms/CMSApplication.java
@@ -1,4 +1,4 @@
-package pl.com.bottega.dms;
+package pl.com.bottega.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/pl/com/bottega/cms/infrastructure/GlobalParamsAndUtils.java
@@ -0,0 +1,9 @@
+package pl.com.bottega.cms.infrastructure;//tu bedziemy przechowywac statyczne metody/pola finalowe, z ktorych bedzie korzystac aplikacja
+
+/**
+ * Created by ogurekk on 2017-04-09.
+ */
+public class GlobalParamsAndUtils {
+
+
+}
