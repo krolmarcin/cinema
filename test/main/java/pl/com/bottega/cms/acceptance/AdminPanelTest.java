28373180d14ac1700402a1f717471f7901937ba7
ogurekk <karwacki.wojciech@gmail.com>
2017-04-09 11:36:23 +0200

    Configuracja springa - beany do admin panel, cinema repo, movie repo i showing repo

--- test/main/java/pl/com/bottega/cms/acceptance/AdminPanelTest.java
+++ test/main/java/pl/com/bottega/cms/acceptance/AdminPanelTest.java
@@ -1,10 +1,14 @@
 package pl.com.bottega.cms.acceptance;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import pl.com.bottega.cms.model.Cinema;
+
+import static pl.com.bottega.cms.shared.Stubs.*;
 
 /**
  * Created by ogurekk on 2017-04-09.
@@ -16,4 +20,12 @@ public class AdminPanelTest {
 
     @Autowired
     private AdminPanel adminPanel;
+
+    @
+
+    @Test
+    public void shouldCreateCinema() {
+        Cinema cinema = TEST_CINEMA_0;
+
+    }
 }
