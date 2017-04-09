346439e1722fe4791a0699d0c38cf43d92b236cf
MaciejG78 <Maciej_G@poczta.fm>
2017-04-09 12:46:39 +0200

    cinema add

---
+++ src/main/java/pl/com/bottega/cms/infrastructure/JPACinemaCatalog.java
@@ -0,0 +1,16 @@
+package pl.com.bottega.cms.infrastructure;
+
+import pl.com.bottega.cms.application.CinemaCatalog;
+import pl.com.bottega.cms.application.CinemaDto;
+
+import java.util.List;
+
+/**
+ * Created by maciek on 09.04.2017.
+ */
+public class JPACinemaCatalog implements CinemaCatalog {
+    @Override
+    public List<CinemaDto> getCinemas() {
+        return null;
+    }
+}
