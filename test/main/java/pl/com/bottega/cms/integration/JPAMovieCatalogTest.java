5b86563d4cbd8b80bdebaa165589bb7c4d6e8fa0
Marcin <szeryfmk@gmail.com>
2017-04-22 15:52:06 +0200

    see movies available on a given day

--- test/main/java/pl/com/bottega/cms/integration/JPAMovieCatalogTest.java
+++ test/main/java/pl/com/bottega/cms/integration/JPAMovieCatalogTest.java
@@ -1,12 +1,18 @@
 package pl.com.bottega.cms.integration;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import pl.com.bottega.cms.application.MovieDto;
 import pl.com.bottega.cms.infrastructure.GlobalParamsAndUtils;
 import pl.com.bottega.cms.infrastructure.JPAMovieCatalog;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,12 +25,12 @@ public class JPAMovieCatalogTest {
     private JPAMovieCatalog movieCatalog;
 
     private GlobalParamsAndUtils globalParamsAndUtils;
-/*
+
     @Test
     @Sql("/fixtures/cinemaCatalog.sql")
     public void shouldFindMoviesInCinema() {
         Long cinemaId = 1L;
-        LocalDate date = new LocalDate(2017, 04, 20);
+        LocalDate date = LocalDate.of(2017, 04, 20);
 
         List<MovieDto> movies = movieCatalog.listAvailableMovies(cinemaId, date);
 
@@ -32,5 +38,5 @@ public class JPAMovieCatalogTest {
         assertThat(movies.get(0).getTitle()).isEqualTo("Pulp Fiction");
         assertThat(movies.get(1).getTitle()).isEqualTo("Pulp Fiction 2");
     }
-*/
+
 }
